7edbb62f92ea2b0f15a801c57448e1b4fcec11f0
Shakiba Mohammadnia <shakiba.mn@gmail.com>
2025-10-27 13:42:21 +0800

    Add specPattern to Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3010",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    supportFile: "cypress/support/e2e.js"
   },
 });
