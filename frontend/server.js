3771f9ef6f0021f9551483843316dbf41181fbd9
Shakiba Mohammadnia <shakiba.mn@gmail.com>
2025-10-28 17:49:39 +0800

    Fix

--- frontend/server.js
+++ frontend/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 
+const app = express();
 app.use(express.static(path.join(__dirname)));
 
-app.listen(8080, () => {
-  console.log('Frontend server running at http://localhost:8080');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'payment-form.html'));
+});
+
+const PORT = Number(process.env.PORT) || 8080;
+
+process.on('uncaughtException', (err) => {
+  console.error('uncaughtException', err && err.stack ? err.stack : err);
+  process.exit(1);
+});
+process.on('unhandledRejection', (err) => {
+  console.error('unhandledRejection', err && err.stack ? err.stack : err);
+  process.exit(1);
+});
+
+app.listen(PORT, () => {
+  console.log(`Frontend server running at http://localhost:${PORT}`);
 });
